394543ab2c42c691da95a54078a75afb682e8cb9
Oli Young <oliyoung@gmail.com>
2014-03-07 11:53:24 +1030

    Simple UIWebView
    it was good enough for Facebook

--- Musio/MusioViewController.m
+++ Musio/MusioViewController.m
@@ -9,6 +9,7 @@
 #import "MusioViewController.h"
 
 @interface MusioViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *MainWebView;
 
 @end
 
@@ -18,6 +19,10 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSString *fullURL = @"http://beta.musio.co";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_MainWebView loadRequest:requestObj];
 }
 
 - (void)didReceiveMemoryWarning
