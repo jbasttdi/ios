e9905e039f801886589ad824d5c89efe2f7c591a
Oli Young <oliyoung@gmail.com>
2014-05-12 23:07:10 +0930

    Create new Track view class and attach it to the track pane

---
+++ Musio/MusioTrackView.h
@@ -0,0 +1,13 @@
+//
+//  MusioTrackView.h
+//  Musio
+//
+//  Created by Oli Young on 12/05/2014.
+//  Copyright (c) 2014 Musio Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusioTrackView : UIView
+
+@end
---
+++ Musio/MusioTrackView.m
@@ -0,0 +1,31 @@
+//
+//  MusioTrackView.m
+//  Musio
+//
+//  Created by Oli Young on 12/05/2014.
+//  Copyright (c) 2014 Musio Pty Ltd. All rights reserved.
+//
+
+#import "MusioTrackView.h"
+
+@implementation MusioTrackView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
