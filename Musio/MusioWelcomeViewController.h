37118d37e1fce13d144174e733f916fc06934ffb
Oli Young <oliyoung@gmail.com>
2014-05-17 21:15:32 +0930

    Adding WelcomeView controller

---
+++ Musio/MusioWelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  MusioWelcomeViewController.h
+//  Musio
+//
+//  Created by Oli Young on 17/05/2014.
+//  Copyright (c) 2014 Musio Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusioWelcomeViewController : UIViewController <UINavigationControllerDelegate>
+
+@end
