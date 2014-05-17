30e8a512aa7509737cac354625290d6c3b6c6655
Oli Young <oliyoung@gmail.com>
2014-05-17 21:14:29 +0930

    Adding SignUp Controller

---
+++ Musio/MusioSignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  MusioSignUpViewController.h
+//  Musio
+//
+//  Created by Oli Young on 17/05/2014.
+//  Copyright (c) 2014 Musio Pty Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MusioSignUpViewController : UIViewController
+
+@end
---
+++ Musio/MusioSignUpViewController.m
@@ -0,0 +1,60 @@
+//
+//  MusioSignUpViewController.m
+//  Musio
+//
+//  Created by Oli Young on 17/05/2014.
+//  Copyright (c) 2014 Musio Pty Ltd. All rights reserved.
+//
+
+#import "MusioSignUpViewController.h"
+
+@interface MusioSignUpViewController ()
+
+@end
+
+@implementation MusioSignUpViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
